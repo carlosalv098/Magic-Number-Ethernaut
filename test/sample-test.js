a8a6fa0b85dc58f8bd601592aeb9b95dbedc893d
Carlos Benjamin Alvarez Cruz <carlosalvarez098@gmail.com>
2022-02-17 18:44:23 -0600

    Magic Num

--- test/sample-test.js
+++ test/sample-test.js
@@ -1,18 +1,7 @@
 const { expect } = require("chai");
 
-describe("Greeter", function () {
-  it("Should return the new greeting once it's changed", async function () {
-    const Greeter = await ethers.getContractFactory("Greeter");
-    const greeter = await Greeter.deploy("Hello, world!");
-    await greeter.deployed();
+describe("Magic Number", function () {
+  it("Should return the number 42", async function () {
 
-    expect(await greeter.greet()).to.equal("Hello, world!");
-
-    const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
-
-    // wait until the transaction is mined
-    await setGreetingTx.wait();
-
-    expect(await greeter.greet()).to.equal("Hola, mundo!");
   });
 });
